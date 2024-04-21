3d61771951a611ae671fa5866e9a22e72deeb398
podestal <luisrodriguezpodesta@gmail.com>
2024-04-21 10:00:31 -0400

    main structure

---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar"
+import Billing from "./Billing"
+import CarDeal from "./CarDeal"
+import Business from "./Business"
+import Clients from "./Clients"
+import CTA from "./CTA"
+import Stats from "./Stats"
+import Footer from "./Footer"
+import Testimonials from "./Testimonials"
+import Hero from "./Hero"
+export {
+    Navbar,
+    Billing,
+    CarDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero
+}
