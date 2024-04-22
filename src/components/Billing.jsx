0d801578ce5f712d951fe819468349541e131121
podestal <luisrodriguezpodesta@gmail.com>
2024-04-22 12:29:34 -0400

    billing done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,6 +9,19 @@ const Billing = () => {
         </div>
         {/* <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/> */}
+        <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>
+                Easily control your <br className="sm:block hidden"/> billing & invoicing.
+            </h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                With the right credit card, you can improve your financial life by building credit, earning rewards
+                and saving money. But with hundreds of credit cards on the market.
+            </p>
+            <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+                <img src={apple} alt="app_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"/>
+                <img src={google} alt="play_store" className="w-[128px] h-[42px] object-contain cursor-pointer"/>
+            </div>
+        </div>
     </section>
   )
 }
