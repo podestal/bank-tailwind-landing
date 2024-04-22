b75de0a15a24115556720240269c7cc55bcf1e46
podestal <luisrodriguezpodesta@gmail.com>
2024-04-22 12:22:37 -0400

    left side billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { apple, bill, google } from "../assets"
+import styles, { layout } from "../style"
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+        <div className={layout.sectionImgReverse}>
+            <img src={bill} alt="Billing" className="w-[100%] h-[100%] relative" />
+        </div>
+        {/* <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"/>
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/> */}
+    </section>
   )
 }
 
